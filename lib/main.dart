7b88fccc056b02d7e25fc7a7d1ae52b89818f4b8
Albert221 <w.albert221@gmail.com>
2023-04-02 17:21:10 +0200

    Pass Heroku API URL and API key

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,9 @@ import 'package:provider/provider.dart';
 
 import 'screens/previous_entries/bloc/previous_entries_cubit.dart';
 
-const _apiUrl = 'http://localhost:1337/api';
+const _apiUrl = 'https://aw-pilot-helper.herokuapp.com/api';
 const _apiToken =
-    '063ec42189b26037bb64a34d97a18092c7454af099335581725274c2ec201e9c198524a5a164bb355733790827d029a5c6ab2113e4168daad52af1f1c263f08169d40bfd1878fd5816174d2d15499292de3d48bf559e79feffec988490828f1102938320ea1354ec15cf050ab112403fef8d637447325aad37c69bc9bc204e77';
+    'secret';
 
 void main() {
   runApp(
